2f3ed61f8382eaed0d8e6a8db078e985272a2999
bdb2381 <brad.beggs@gmail.com>
2020-09-01 11:59:44 -0700

    Brad Beggs Attempt 1

--- README.md
+++ README.md
@@ -1,27 +1,28 @@
 # Flatagram
 
-Today you'll be building an app for viewing, liking, and commenting on a single picture. You will be using a local API and building out the frontend for our app, Flatagram.
+x Today you'll be building an app for viewing, liking, and commenting on a single picture. You will be using a local API and building out the frontend for our app, Flatagram.
 
 ## Demo
 
 Use this gif as an example of how the app should work.
-
+x 
 ![demo gif](assets/demo.gif)
 
 ## Setup
 
-- Fork and clone this repository
-- Run `json-server --watch db.json --routes routes.json` to get the backend started
-- Open the `index.html` file on your browser
+- x Fork and clone this repository
+- x Run `json-server --watch db.json --routes routes.json` to get the backend started
+- x Open the `index.html` file on your browser
 
 ## Endpoints
 
-Your base URL for your API will be: http://localhost:3000
+x Your base URL for your API will be: 
+http://localhost:3000
 
-The endpoints you will need are:
+x The endpoints you will need are:
 
 - GET `/images/1`
-- PATCH `/images/1`
+- x PATCH `/images/1`
 - POST `/comments`
 - DELETE `/comments/:id`
 
@@ -29,25 +30,27 @@ The endpoints you will need are:
 
 As a user, I can:
 
-- See the image received from the server, including its title, likes and comments when the page loads
-- Click on the heart icon to increase image likes, and still see them when I reload the page
-- Add a comment (no persistance needed)
+- 1) See the image received from the server, 
+x including its title, 
+x likes 
+x comments   (mostly done)
+when the page loads
+
+- x 2) Click on the heart icon to increase image likes, and still see them when I reload the page
+
+- 3) Add a comment (no persistance needed)
+
 
 ## Advanced Deliverables
-
-These deliverables are not required to pass the code challenge, but if you have the extra time, or even after the code challenge, they are a great way to stretch your skills.
-
-> Note: If you are going to attempt these advanced deliverables, please be sure to have a working commit with all the Core Deliverables first!
 
 As a user, I can:
 
 - Downvote an image
+
 - Still see the comments written after reloading the page
   > For this one, you want to make a POST request to the `/comments` endpoint.
   > Your comment object must have an `imageId` key with a value of `1` for it to work.
+
 - Delete a comment
   > To persist this, you will have to make a DELETE request to the `/comments/:id` endpoint.
 
-## Rubric
-
-You can find the rubric for this assessment [here](https://github.com/learn-co-curriculum/se-rubrics/blob/master/module-3.md).
--- src/index.js
+++ src/index.js
@@ -1,1 +1,152 @@
 // write your code here
+
+///////////////////
+//function calls
+likesListner() 
+getImages()
+getComments()  
+
+///////////////////
+//listening functions
+function likesListner(){
+  heartElement = document.getElementsByClassName('like-button')[0] //find the heart icon/button
+
+  heartElement.addEventListener('click', handleLikes) //attach listen for click and callback function handleLikes
+}
+
+function newCommentListner(){
+  commentForm = document.getElementById("form")
+  commentForm.addEventListener('submit', renderNewComment)  // shortcuting process for time ;) 
+}
+
+
+///////////////////
+//handling function
+function handleComments(comments){
+  comments.forEach(comment => {
+    renderComments(comment) //send each comment  to get render
+  })
+} 
+
+
+function handleImages(images){
+  images.forEach(image => {
+    renderCard(image)  //for each image/card in db display the images/card items. Comments displayed via another function
+  }) 
+}
+
+function handleLikes(event){
+  event.preventDefault 
+  
+  likesNumbersElement = event.target.previousElementSibling //get the likes element
+
+  likesText = likesNumbersElement.innerText //get just the text e.g. "3 likes"
+  newLikeNumber = parseInt(likesText) //get & make just an integer 
+
+
+  if(event.target.matches("button.like-button"))
+  {
+    newLikeNumber += 1
+    patchLikesNumber(newLikeNumber, event)
+  }
+  
+  // code below for removing a like. needs listener and dislike button  
+  // if(event.target.matches("button.dislike-button")
+  // ){
+  //   newLikeNumber = newLikeNumber - 1
+  //   patchLikesNumber(newLikeNumber, event)
+  // }
+
+
+
+}
+
+
+///////////////////
+//fetch calls
+function getComments(){
+  //this gets _all_ images, event just one
+fetch('http://localhost:3000/comments')
+.then(response => response.json())
+.then(comments => 
+  handleComments(comments)
+  );
+} 
+
+function getImages(){
+//this gets _all_ images, event just one
+fetch('http://localhost:3000/images')
+  .then(response => response.json())
+  .then(images => 
+    handleImages(images)
+    );
+
+
+}
+
+function patchLikesNumber(newLikeNumber, event){
+  //options to update the json DB with likes
+  options = {
+    method: 'PATCH', 
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      "likes": `${newLikeNumber}`
+    })
+  }
+
+ 
+ fetch(`http://localhost:3000/images/${event.target.parentElement.parentElement.id}`, options)
+ .then(resp => resp.json())
+ .then(image => {
+   console.log(image) //images data from DB
+   renderLikesToDom(image, event)  //pesssmistic render
+ } ) 
+}
+
+
+
+
+
+
+///////////////////
+//DOM Manipulation functions
+function renderNewComment(event){
+  event.preventDefault
+  // if(event.preventDefault.matches....)
+  // code not complete. almost. ran out of time., 
+}
+
+
+function renderComments(comments){
+console.log(comments)
+
+  document.getElementsByClassName("comments")[0].innerHTML = `<li>${comments.content}<li>`
+
+  //need to load all comments. picked wrong location for the loop
+}
+
+
+function renderCard(image){
+  cardElement = document.getElementById(image.id) //locate the card in order to add the json to it
+
+  cardElement.firstElementChild.innerText = image.title  //update tile 
+
+  cardElement.getElementsByTagName("img")[0].src = image.image  //update image 
+
+  cardElement.getElementsByClassName("likes")[0].innerText = `${image.likes} likes`  //update likes from db
+
+  
+
+}
+
+function renderLikesToDom(image, event){
+   
+  likesNumbersElement = event.target.previousElementSibling //get the likes element
+
+  likesNumbersElement.innerText = `${image.likes} likes` //update the DOM with number and text
+
+
+}
+
